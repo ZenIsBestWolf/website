99f52ae00c0690dc8bdf10d8e9f0bf2642699d76
Zen Wolf <zenisbestwolf@gmail.com>
2022-03-25 23:09:42 -0400

    add hue

---
+++ hue/javascript/navigation.js
@@ -0,0 +1,16 @@
+
+function about() {
+    document.getElementById("bio").style.display="block";
+    document.getElementById("whatHaveYouDone").style.display="none";
+    document.getElementById("socials").style.display="none";
+}
+function whathaveyoudone() {
+    document.getElementById("bio").style.display="none";
+    document.getElementById("whatHaveYouDone").style.display="block";
+    document.getElementById("socials").style.display="none";
+}
+function socials() {
+    document.getElementById("bio").style.display="none";
+    document.getElementById("whatHaveYouDone").style.display="none";
+    document.getElementById("socials").style.display="block";
+}
